3f9659224785b9923ea7ec2ffc111ec9fe6c9ec6
misbah <findmisbah2017@gmail.com>
2019-04-30 10:03:16 +0530

    commit
    Change-Id: Ica26654a7daafbd0587aed5e31fc4096608ae3e8

--- Hello1.java
+++ Hello1.java
@@ -3,8 +3,13 @@ public class  Hello1{
 	public static void main(String[] args) {
 		for (int i = 0;i< 10 ;i++ ) {
 
+<<<<<<< HEAD
 			String a = "branch b";
 		}
+=======
+			String a = "branch master;
+	t 10:02	}
+>>>>>>> 9820f8f... commit
 	}
 
 
