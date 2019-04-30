6469c52b6c83721b6b93305f3566cfaf85e8b8a3
misbah <findmisbah2017@gmail.com>
2019-04-30 10:36:45 +0530

    commit
    Change-Id: Ib5f19ddd2eaf6e4bda8d0321973aec0706047429

--- Hello1.java
+++ Hello1.java
@@ -4,10 +4,14 @@ public class  Hello1{
 		for (int i = 0;i< 10 ;i++ ) {
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 			String a = "branch b";
 =======
 			String a = "branch imaster 10:2=30";
 >>>>>>> 09d813f... commmit
+=======
+			String a = "branch imaster 10:37";
+>>>>>>> 9370d81... commit
 		}
 	}
 
