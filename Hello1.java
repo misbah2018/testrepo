d4a20eca8474c3d3e74af8175b7cefe7306b9c0a
Misbah Saad <findmisbah2017@gmail.com>
2019-05-09 08:40:54 +0530

    commit
    Change-Id: Ibbc12c8fe09e2d84340f4b3133c76bc7c6c46b3a

--- Hello1.java
+++ Hello1.java
@@ -3,7 +3,11 @@ public class  Hello1{
 	public static void main(String[] args) {
 		for (int i = 0;i< 10 ;i++ ) {
 
+<<<<<<< HEAD
 			String a = "branch b 9:59";
+=======
+			String a = "branch b1 new1";
+>>>>>>> 0cdefef... commit
 		}
 	}
 
